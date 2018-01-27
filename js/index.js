495b544674b95b4f4208276eb926322501124108
RileyDatLord <rileydatlord@gmail.com>
2018-01-26 18:58:08 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,7 @@ var dateNow = new Date()
 
 main.innerHTML = dateNow.getHours() - 12 + ":" + dateNow.getMinutes() + ":" + dateNow.getSeconds()
 main.style.color = "red"
+setInterval(function() {
+  main.style.color = "blue"
+  setTimeout(function(){main.style.color = "red"},500)
+},1000)
