fdfed33edd012165bcaf08114a68cad03140546f
RileyDatLord <rileydatlord@gmail.com>
2018-01-26 18:54:28 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var main = document.getElementById("main")
 function times(x,y){var z = x*y}
+
 var dateNow = new Date()
 
 main.innerHTML = dateNow.getHours() + ":" + dateNow.getMinutes() + ":" + dateNow.getSeconds()
