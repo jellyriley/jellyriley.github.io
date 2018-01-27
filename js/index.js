e16f9fb2762a4e86ce2ab286789d2abc2d3be105
RileyDatLord <rileydatlord@gmail.com>
2018-01-26 19:00:10 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 var pst = document.getElementById("pst")
 var dateNow = new Date()
 
+pst.innerHTML = "..."
+
 pst.innerHTML = dateNow.getHours() - 12 + ":" + dateNow.getMinutes() + ":" + dateNow.getSeconds()
 pst.style.color = "red"
 pst.innerHTML += "<br>PST ^^"
