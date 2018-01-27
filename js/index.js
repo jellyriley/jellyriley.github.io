e35844a0d69251302e45ea823dad10f4c5c6ebee
RileyDatLord <rileydatlord@gmail.com>
2018-01-26 18:54:13 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var main = document.getElementById("main")
 function times(x,y){var z = x*y}
+var dateNow = new Date()
 
-main.innerHTML = new Date().getDay()
+main.innerHTML = dateNow.getHours() + ":" + dateNow.getMinutes() + ":" + dateNow.getSeconds()
 main.style.color = "red"
